26323ebee766f240eafab528684af494c3556c50
Wagner da Silva Aquino Junior <wagner.aquino@tecnospeed.com.br>
2023-04-19 21:48:16 -0300

    soma das peças ao clicar em somar ou subtrair

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-const robotron = document.querySelector('#robotron');
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
 
 //outra forma de declarar funcao anonima: () => {}
-robotron.addEventListener("click", function(evento){
-    console.log("Cliquei no robô.");
-});
+//somar.addEventListener("click", function(){manipulaDados("somar")});
 
-function dizOi(nome){
-    console.log('Oi ' + nome);
-}
 
-dizOi("Wagner");
+function manipulaDados(operacao){
+    if(operacao === '-'){
+        braco.value = parseInt(braco.value) - 1;
+    } else{
+        braco.value = parseInt(braco.value) + 1;
+    }
+};
